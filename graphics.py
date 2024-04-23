9f6bef9e1328bb62afe011fd977eb469cba58392
VanUffelenAntoine <antoine@mansio.be>
2024-04-23 11:26:11 +0200

    added line drawing

--- graphics.py
+++ graphics.py
@@ -15,9 +15,29 @@ class Window():
 
     def wait_for_close(self):
         self.__running = True
-        while self.redraw():
+        while self.__running:
             self.redraw()
         print('Window closed...')
     
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_col="white"):
+        line.draw(self.__canvas, fill_col)
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color):
+            canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y,
+                               fill= fill_color, width= 2)
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800,600)
+    line1 = Line(Point(0,0),Point(99, 99))
+    win.draw_line(line1)
+
     win.wait_for_close()
 
 main()
