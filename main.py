3f9cfc82c50bcd41b3ec9e897b16cd6f02d19955
VanUffelenAntoine <antoine@mansio.be>
2024-04-23 12:33:42 +0200

    Added maze drawing

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from cell import Cell
 from graphics import Window, Line, Point
+from maze import Maze
 
 def main():
-    win = Window(800,600)
+    # win = Window(800,600)
     # line1 = Line(Point(0,0),Point(99, 99))
     # win.draw_line(line1)
 
-    cell1 = Cell(win)
-    cell1.draw(50, 50, 100, 100)
+    # cell1 = Cell(win)
+    # cell1.draw(50, 50, 100, 100)
 
-    cell2 = Cell(win)
-    cell2.draw(100, 50, 150, 100)
+    # cell2 = Cell(win)
+    # cell2.draw(100, 50, 150, 100)
 
-    cell1.draw_move(cell2)
+    # cell1.draw_move(cell2)
+
+    num_row = 12
+    num_col = 10
+    margin = 1
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_col
+    cell_size_y = (screen_y - 2 * margin) / num_row
+    win = Window(screen_x, screen_y)
+    maze = Maze(margin, margin, num_row, num_col, cell_size_x, cell_size_y, win)   
 
     win.wait_for_close()
 
---
+++ maze.py
@@ -0,0 +1,36 @@
+import time
+
+from cell import Cell
+
+class Maze():
+    def __init__(self, x1, y1, num_rows, num_cols, cell_s_x, cell_s_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.c_s_x = cell_s_x
+        self.c_s_y = cell_s_y
+        self.win = win
+        self._cells = []
+        self.__create_cells()
+
+    def __create_cells(self):
+        for i in range(1, self.num_rows):
+            row = []
+            for j in range(1, self.num_cols):
+                    row.append(self.__draw_cells(i,j))
+            self._cells.append(row)
+
+    def __draw_cells(self, i, j):
+        cell = Cell(self.win)
+        x_t_left = self.x1 + i * self.c_s_x
+        y_t_left = self.y1 + j * self.c_s_y
+        cell.draw(x_t_left, y_t_left, x_t_left + self.c_s_x, y_t_left + self.c_s_y)      
+        self.__animate()
+        return cell
+    
+    def __animate(self):
+        if self.win is None:
+            return
+        self.win.redraw()
+        time.sleep(0.05)
